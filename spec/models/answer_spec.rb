8c3bd47fd587233e09f59c2dd5a6749171fd4884
Dmitry Lyutsko <dmitry.lyutsko@yandex.ru>
2022-11-09 21:59:07 +0300

    Dropped redundant tests

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it { should have_db_column :question_id }
-  it { should have_db_index :question_id }
-
   it { should belong_to :question }
 
   it { should validate_presence_of :body }

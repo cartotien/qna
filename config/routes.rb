44a83e86bb698b31ddeb17e7113dabd517db6c13
Dmitry Lyutsko <dmitry.lyutsko@yandex.ru>
2022-11-11 14:44:58 +0300

    Adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "questions#index"
 
-  resources :questions, shallow: true do
-    resources :answers, only: %i[new create edit update destroy]
+  resources :questions, only: %i[index show new create], shallow: true do
+    resources :answers, only: %i[new create]
   end
 end
